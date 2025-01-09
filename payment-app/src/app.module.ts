414949e548de4c67e179c270fb440e9f3af34b9b
Ahsanul Hoque Famid <ahsanulhoque721@gmail.com>
2025-01-09 16:02:49 +0600

    set up order-app and payment-app

---
+++ payment-app/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AppConfigModule } from './config/app/config.module';
+import { PostgresDatabaseProviderModule } from './provider/database/postgres/provider.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Make config global
+    AppConfigModule,
+    // Database connection
+    PostgresDatabaseProviderModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ payment-app/src/main.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AppConfigService } from './config/app/config.service';
+import { RequestMethod, ValidationPipe, VersioningType } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
+import {
+  ValidationException,
+  ValidationFilter,
+} from './common/filters/validation.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const appConfig: AppConfigService = app.get(AppConfigService);
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: appConfig.version,
+  });
+
+  // validation filters
+  app.useGlobalFilters(new ValidationFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: false,
+      whitelist: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const errMsg = {};
+        errors.forEach((err) => {
+          errMsg[err.property] = [...Object.values(err.constraints)];
+        });
+        return new ValidationException(errMsg);
+      },
+    }),
+  );
+  console.log("PORT: ", appConfig.port);
+  await app.listen(appConfig.port?? 3003);
+}
+bootstrap();
